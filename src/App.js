9313178e070a3c900c3c6dbf56f103201a68c1ed
Khalid-MK <khalidabdelnaser382@gmail.com>
2021-04-07 16:29:17 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import {
+    HubConnectionBuilder,
+    LogLevel,
+    HttpTransportType,
+} from "@microsoft/signalr";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [connection, setConnection] = useState(null);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function getUsers() {
+            let users = await axios
+                .get("https://jsonplaceholder.typicode.com/users")
+                .then(({ data }) => {
+                    return data;
+                });
+
+            setData(users);
+        }
+
+        getUsers();
+
+        return () => {};
+    }, []);
+
+    useEffect(() => {
+        // Define Connection
+        const newConnection = new HubConnectionBuilder()
+            .configureLogging(LogLevel.Debug)
+            .withUrl("https://localhost:5001/hubs/dataHub", {
+                skipNegotiation: true,
+                transport: HttpTransportType.WebSockets,
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(newConnection);
+
+        return () => {};
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            // Start Connection
+            connection
+                .start()
+                // Define CallBack Function
+                .then((result) => {
+                    console.log("Connected!");
+                    // Register Subscribed Events
+                    connection.on("ReceiveData", (data) => {
+                        console.log(data);
+                    });
+                    connection.on("ConnectedMessage", (data) => {
+                        console.log(data);
+                    });
+                    connection.on("RecordDeleted", (data) => {
+                        console.log(data);
+                    });
+                })
+                .catch((e) => console.log("Connection failed: ", e));
+        }
+    }, [connection]);
+
+    // Call Server Function
+    const handleDleteClick = async (user) => {
+        if (connection.connectionStarted) {
+            try {
+                // Invoke Server Method.
+
+                // await axios.post(
+                //     "https://localhost:5001/api/data/insertdata",
+                //     JSON.stringify(user),
+                //     {
+                //         headers: {
+                //             "Content-Type": "application/json",
+                //         },
+                //     }
+                // );
+
+                axios.delete(`https://localhost:5001/api/data/${user.id}`);
+
+                // await connection.invoke("sendData", { id: 2, name: "khalid" });
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
+            alert("No connection to server yet.");
+        }
+    };
+
+    return (
+        <div className="App">
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>UserName</th>
+                    <th>Email</th>
+                </tr>
+                {data.map((user, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td className="text-center">
+                                <button
+                                    className="btn btn-danger"
+                                    onClick={(e) => handleDleteClick(user)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </table>
+        </div>
+    );
 }
 
 export default App;
